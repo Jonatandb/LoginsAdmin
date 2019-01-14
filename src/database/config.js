17add087f491e20b921a5a0cfc9ef430d1db18a1
Jonatandb <jonatandb@gmail.com>
2019-01-14 01:59:51 -0300

    Agrego archivo de configuración con endpoints

---
+++ src/database/config.js
@@ -0,0 +1,6 @@
+
+const loginsServiceNameEndpoint = "https://jonatandb01.000webhostapp.com/loginsservicename";
+const loginsDataEndpoint = "https://jonatandb01.000webhostapp.com/logindata?id=1";
+
+module.exports = loginsDataEndpoint;
+module.exports = loginsServiceNameEndpoint;
