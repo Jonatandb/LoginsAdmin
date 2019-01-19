f6fa56695f59ebae110587ee033420b9c68c01b2
Jonatandb <jonatandb@gmail.com>
2019-01-18 21:01:31 -0300

    Prueba de conección a la base de datos Postgress

--- src/database/index.js
+++ src/database/index.js
@@ -1,19 +1,35 @@
-import axios from "axios";
-import loginsServiceNameEndpoint from './config.js';
-
+// import axios from "axios";
+// import loginsServiceNameEndpoint from './config.js';
+import { Client } from "pg";
 const showLogs = false;
 // https://www.robinwieruch.de/react-fetching-data/
 // TODO: Mejorar esto!
 export default {    
     async getLoginsServiceName() {
-        const url = loginsServiceNameEndpoint;
-        try {
-            const result = await axios.get(url);
-showLogs && console.log(result.data);
-            return result.data;
-        }
-        catch (error) {
-            return error;
-        }
+
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL,
+            ssl: true,
+        })
+
+        client.connect();
+
+        client.query('', (err, resp) => {
+            if(err) throw err;
+            for(let row of res.rows) {
+                console.log(JSON.stringify(row));
+            }
+            client.end();
+        });
+//         const url = loginsServiceNameEndpoint;
+//         try {
+//             const result = await axios.get(url);
+// showLogs && console.log(result.data);
+//             return result.data;
+//         }
+//         catch (error) {
+//             return error;
+//         }
+
     }
 }
