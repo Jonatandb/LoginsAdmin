940fb8494473aa7ecc4fcb14f43de043f35e91a0
Jonatandb <jonatandb@gmail.com>
2019-01-24 02:00:43 -0300

    Comento todo, en Heroku no funcionaaaa

--- src/database/index.js
+++ src/database/index.js
@@ -1,20 +1,21 @@
 //require('dotenv').config(); // lee y establece lo que tenga el archivo ".env" (Usado solo en dev)
-const promise = require('bluebird');
-const initOptions = {promiseLib: promise};
-const pgp = require('pg-promise')(initOptions);
+// const promise = require('bluebird');
+// const initOptions = {promiseLib: promise};
+// const pgp = require('pg-promise')(initOptions);
 const connectionString = process.env.DATABASE_URL;
 
 export default {
     async getLoginsServiceName() {
-        const db = pgp(connectionString);
-        db.any('select login_id, service_name from logins;')
-        .then(data => {
-            return data;
-        })
-        .catch(error => {
-            console.log('ERROR:', error);
+        // const db = pgp(connectionString);
+        // db.any('select login_id, service_name from logins;')
+        // .then(data => {
+        //     return data;
+        // })
+        // .catch(error => {
+        //     console.log('ERROR:', error);
+        console.log(connectionString);
             return [];
-        })
-        .finally(db.$pool.end);
+        // })
+        // .finally(db.$pool.end);
     }
 }
